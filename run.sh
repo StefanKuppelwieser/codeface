c8faa7455d82db1cbe114d6a0740a60080290f80
Claus Hunsen <hunsen@fim.uni-passau.de>
2016-11-14 16:40:15 +0100

    Add cluster scripts
    To let Codeface run on Slurm infrastructure, some scripts are
    neccessary.
    
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

---
+++ run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd ${DIR} > /dev/null
+
+    # logging
+    echo =================================================================
+    echo "Calling codeface  with following arguments:"
+    echo "$@"
+    echo =================================================================
+    echo
+
+    # get parameters from command line
+    TMPDIR=$1 # currently ignored
+    CFCONF=$2
+    CSCONF=$3
+    REPOS=$4
+    MAILINGLISTS=$5
+    RESULTS=$6
+    LOGS=$7
+
+    CFDIR="/mnt/codeface"
+    CFDATA="/mnt/codeface-data"
+    CFEXTRACT="/mnt/codeface-extraction"
+
+    ## create log folder
+    mkdir -p ${LOGS}
+
+    pushd $CFDIR
+
+          ## start ID service
+          pushd "id_service"
+                echo "### " $(date "+%F %T") "Starting ID service" 2>&1 > "${LOGS}/id_service.log"
+                nodejs id_service.js ${CFCONF} "info" 2>&1 >> "${LOGS}/id_service.log" &
+                IDSERVICE=$!
+          popd
+
+          ## run codeface analysis with current tagging set
+          codeface -j 11 -l "devinfo" run --recreate -c ${CFCONF} -p ${CSCONF} ${RESULTS} ${REPOS} > ${LOGS}/codeface_run.log 2>&1
+
+          # run mailing-list analysis (attached to feature/proximity analysis!)
+          #codeface -j 11 -l "devinfo" ml -c ${CFCONF} -p ${CSCONF} "${RESULTS}" "${MAILINGLISTS}" > ${LOGS}/codeface_ml.log 2>&1
+          #codeface -j 11 -l "devinfo" ml --use-corpus -c ${CFCONF} -p ${CSCONF} "${RESULTS}" "${MAILINGLISTS}" > ${LOGS}/codeface_ml.log 2>&1
+
+          # run extraction process for this configuration
+          pushd "${EXTRACTION_PATH}" > /dev/null
+                EXTRACTION="${CFEXTRACT}/run-extraction.py"
+                python ${EXTRACTION} -c ${CFCONF} -p ${CSCONF} ${RESULTS} > ${LOGS}/codeface_extraction.log 2>&1
+          popd
+
+
+          ## stop ID service
+          kill $IDSERVICE
+
+    popd
+
+popd > /dev/null
